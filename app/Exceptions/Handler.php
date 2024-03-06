189607992c90a64b4acd9904187d859653ffb47c
Irwing Naranjo <khalisser@gmail.com>
2024-03-06 10:53:45 -0300

    Refactor exception handling in Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -24,7 +24,7 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            if ($this->app->environment('production')) {
+            if (app()->environment('production') && $this->shouldReport($e)) {
                 if (app()->bound('sentry')) {
                     app('sentry')->captureException($e);
                 }
