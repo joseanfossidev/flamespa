b4424f09b0867f97313b9bd0526046adc182f40e
Irwing Naranjo <khalisser@gmail.com>
2024-03-15 20:42:15 -0300

    Add auditing functionality to the application

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
+        OwenIt\Auditing\AuditingServiceProvider::class,
     ])->toArray(),
 
     /*
