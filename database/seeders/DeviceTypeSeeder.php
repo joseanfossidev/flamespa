74a7391934d7cd0f1db3f2a67e1a419181e9b507
Irwing Naranjo <khalisser@gmail.com>
2024-03-15 23:57:23 -0300

    Add predefined device types to DeviceTypeSeeder

--- database/seeders/DeviceTypeSeeder.php
+++ database/seeders/DeviceTypeSeeder.php
@@ -2,15 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tracking\Configurations\DeviceType;
 use Illuminate\Database\Seeder;
 
 class DeviceTypeSeeder extends Seeder
 {
+    private $records = [
+        ['name' => 'GPS'],
+        ['name' => 'Tablet'],
+        ['name' => 'DVR'],
+        ['name' => 'MDVR'],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        \App\Models\Tracking\Configurations\DeviceType::factory(100)->create();
+        foreach ($this->records as $record) {
+            $deviceType = new DeviceType;
+            $deviceType->name = $record['name'];
+            $deviceType->save();
+        }
     }
 }
