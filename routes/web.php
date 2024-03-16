b031adeb68624631e28a83f2f6efcdd81ba13046
Irwing Naranjo <khalisser@gmail.com>
2024-03-16 00:02:32 -0300

    Remove unused import statement in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Tracking\Configurations\DevicesTypesController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
