446803a547e020ac84164d4b133fee44d1636ec7
Irwing Naranjo <khalisser@gmail.com>
2024-03-23 15:03:21 -0300

    Refactor routes/api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Jobs\ProcessTransmition;
+use App\Models\Tracking\Configurations\Device;
+use App\Models\Tracking\Configurations\Transmition;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Models\Tracking\Configurations\Transmition;
-use App\Models\Tracking\Configurations\Device;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,13 +13,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('device', function () {
     $device = new Device;
     $device->device_type_id = 1;
-    $device->name = "GV55W001";
-    $device->number_avl = "AVL001";
-    $device->number_imei = "IMEI001";
-    $device->phone_number = "+569001";
-    $device->network_provider = "Claro";
-    $device->network_bandwith = "50MB";
-    $device->provider_supplier = "Global Network";
+    $device->name = 'GV55W001';
+    $device->number_avl = 'AVL001';
+    $device->number_imei = 'IMEI001';
+    $device->phone_number = '+569001';
+    $device->network_provider = 'Claro';
+    $device->network_bandwith = '50MB';
+    $device->provider_supplier = 'Global Network';
     $device->save();
 
     return response()->json(['device' => $device]);
@@ -28,16 +27,19 @@ Route::get('device', function () {
 
 Route::get('transmitions', function () {
     $transmitionsCount = Transmition::count();
+
     return response()->json(['transmitions' => $transmitionsCount]);
 });
 
 Route::post('transmitions', function () {
     $data = request()->all();
     ProcessTransmition::dispatch($data);
-    return response()->json(['message' => 'Transmition created ' . $data['imei']], 201);
+
+    return response()->json(['message' => 'Transmition created '.$data['imei']], 201);
 });
 
 Route::post('transmitions-empty', function () {
     $data = request()->all();
-    return response()->json(['message' => 'Transmition created ' . $data['imei']], 201);
+
+    return response()->json(['message' => 'Transmition created '.$data['imei']], 201);
 });
